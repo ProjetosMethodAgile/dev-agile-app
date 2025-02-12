de504401c816a504a0a6a5dde22218f1623da190
Felipe Johnny <felipijohnny@outlook.com>
2025-02-12 11:13:48 -0300

    aplicando distinção de empresas ao logar

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ require("dotenv").config(); // Carrega as variáveis de ambiente do arquivo .env
 const app = require("./src/app.js"); // Importa a aplicação Express
 
 const PORT = 3000;
-// const https = require("https");
+const https = require("https");
 
-// const fs = require("fs");
+const fs = require("fs");
 
-// const httpsOptions = {
-//   key: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/privkey.pem"),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/fullchain.pem"),
-// };
+const httpsOptions = {
+  key: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/fullchain.pem"),
+};
 
-// const server = https.createServer(httpsOptions, app);
+const server = https.createServer(httpsOptions, app);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("servidor de aplicação ligado na porta " + PORT);
 });
