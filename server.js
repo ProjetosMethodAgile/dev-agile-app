614d5bb362c95f2fe07058eee3d46c811aa5ef99
Felipe Johnny <felipijohnny@outlook.com>
2025-02-24 10:03:28 -0300

    aplicando alterações nas permissoes das telas com subtelas

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ require("dotenv").config(); // Carrega as variáveis de ambiente do arquivo .env
 const app = require("./src/app.js"); // Importa a aplicação Express
 
 const PORT = 3000;
-// const https = require("https");
+const https = require("https");
 
-// const fs = require("fs");
+const fs = require("fs");
 
-// const httpsOptions = {
-//   key: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/privkey.pem"),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/fullchain.pem"),
-// };
+const httpsOptions = {
+  key: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/fullchain.pem"),
+};
 
-// const server = https.createServer(httpsOptions, app);
+const server = https.createServer(httpsOptions, app);
 
 app.listen(PORT, () => {
   console.log("servidor de aplicação ligado na porta " + PORT);
