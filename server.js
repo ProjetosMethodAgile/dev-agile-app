3b92959e0841aaf38063ec05f7589071caa1f795
Feljohnny <felipijohnny@outlook.com>
2025-03-21 11:45:21 -0300

    aplicando ajuste no cadastro dos cards

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ require("dotenv").config();
 const app = require("./src/app.js");
 
 const PORT = 3001;
-const https = require("https");
+// const https = require("https");
 
-const fs = require("fs");
+// const fs = require("fs");
 
-const httpsOptions = {
-  key: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/fullchain.pem"),
-};
+// const httpsOptions = {
+//   key: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/privkey.pem"),
+//   cert: fs.readFileSync("/etc/letsencrypt/live/devagile.com.br/fullchain.pem"),
+// };
 
-const server = https.createServer(httpsOptions, app);
+// const server = https.createServer(httpsOptions, app);
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("servidor de aplicação ligado na porta " + PORT);
 });
