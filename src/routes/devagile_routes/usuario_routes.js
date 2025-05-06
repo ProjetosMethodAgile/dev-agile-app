ec7ab6b7302c9b83fb9ff9b003b1670d682f8a19
otaviosouza21 <otaviosouzalu@gmail.com>
2025-05-05 21:42:30 -0300

    mudando de post para put

--- src/routes/devagile_routes/usuario_routes.js
+++ src/routes/devagile_routes/usuario_routes.js
@@ -10,6 +10,10 @@ route.put("/api/usuario/:id", checkTokenLogin, (req, res) =>
   usuario_controller.atualizaUsuario_Controller(req, res)
 );
 
+route.put("/api/usuario/reset-password/:id", checkTokenLogin, (req, res) =>
+  usuario_controller.resetaSenhaUsuario_Controller(req, res)
+);
+
 route.get("/api/usuarios", checkTokenLogin, (req, res) =>
   usuario_controller.pegaTodosUsuarios_Controller(req, res)
 );
@@ -28,10 +32,6 @@ route.post("/api/usuario/login", (req, res) => {
 
 route.post("/api/usuario/register", checkTokenLogin, (req, res) =>
   usuario_controller.registerUsuario_Controller(req, res)
-);
-
-route.post("/api/usuario/reset-password/:id", checkTokenLogin, (req, res) =>
-  usuario_controller.resetaSenhaUsuario_Controller(req, res)
 );
 
 route.delete("/api/usuario/:id", checkTokenLogin, (req, res) => {
