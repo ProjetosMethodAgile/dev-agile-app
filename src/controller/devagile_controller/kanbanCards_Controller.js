84c94166316bec350f752d05fc2ef94e104a2ab4
Feljohnny <felipijohnny@outlook.com>
2025-05-06 10:21:59 -0300

    ajustando coluna de criacao de usuario

--- src/controller/devagile_controller/kanbanCards_Controller.js
+++ src/controller/devagile_controller/kanbanCards_Controller.js
@@ -147,7 +147,7 @@ class KanbanCards_Controller extends Controller {
       }
       const setor = setorResult.setor;
       const column = await devAgile.KanbanComlumns.findOne({
-        where: { setor_id, posicao: "0" },
+        where: { setor_id, posicao: "1" },
       });
       if (!column) {
         return res.status(404).json({
