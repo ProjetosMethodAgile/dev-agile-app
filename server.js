7c61f2e8093a85a6c43ada09224f6b30feb8150d
Feljohnny <felipijohnny@outlook.com>
2025-05-15 13:24:11 -0300

    aplicando log nos emails

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // server.js
 require("dotenv").config();
 const fs = require("fs");
-const https = require("https");
+const https = require("http");
 const app = require("./src/app.js");
 const { initWsServer } = require("./src/websocket.js");
 
 const PORT = 3001;
 const httpsOptions = {
-  key: fs.readFileSync(process.env.LETSENCRIPT_PRIVKEY),
-  cert: fs.readFileSync(process.env.LETSENCRIPT_FULLCHAIN),
+  // key: fs.readFileSync(process.env.LETSENCRIPT_PRIVKEY),
+  // cert: fs.readFileSync(process.env.LETSENCRIPT_FULLCHAIN),
 };
 
 const server = https.createServer(httpsOptions, app);
